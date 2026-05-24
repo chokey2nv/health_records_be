6fbc99842c2a870d25ecf6a378ecc43a9a173893
Agu Chijioke <chokey2nv@gmail.com>
2026-05-24 17:38:58 +0100

    updated

--- start.bat
+++ start.bat
@@ -1,15 +1,3 @@
-@REM set "params=%*"
-@REM cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
-@REM TASKLIST /fi "imagename eq mongod.exe" GOTO app
-
-@REM START /B mongod
-
-@REM :app
-
-@REM pm2 start server.js --name hms
-
-@REM start chrome http://192.168.0.10:8003/
-
 @echo off
 setlocal enabledelayedexpansion
 
@@ -32,7 +20,21 @@ echo Starting HMS Environment
 echo =====================================
 
 :: -------------------------------------------------
-:: MongoDB
+:: MongoDB Config
+:: -------------------------------------------------
+set "MONGO_PATH=C:\Program Files\MongoDB\Server\8.0\bin\mongod.exe"
+set "MONGO_DBPATH=C:\data\db"
+
+:: -------------------------------------------------
+:: Ensure DB Path Exists
+:: -------------------------------------------------
+if not exist "%MONGO_DBPATH%" (
+    echo Creating MongoDB data directory...
+    mkdir "%MONGO_DBPATH%"
+)
+
+:: -------------------------------------------------
+:: Check MongoDB
 :: -------------------------------------------------
 tasklist /FI "IMAGENAME eq mongod.exe" 2>NUL | find /I "mongod.exe" >NUL
 
@@ -40,40 +42,28 @@ if %errorLevel% neq 0 (
 
     echo Starting MongoDB...
 
-    :: CHANGE THIS TO YOUR REAL DB PATH
-    set MONGO_DBPATH=C:\data\db
-
-    start "MongoDB" cmd /k ^
-    ""C:\Program Files\MongoDB\Server\8.0\bin\mongod.exe" --dbpath "!MONGO_DBPATH!""
+    start "MongoDB" "%MONGO_PATH%" --dbpath "%MONGO_DBPATH%"
 
 ) else (
     echo MongoDB already running.
 )
 
 :: -------------------------------------------------
-:: Wait a bit
+:: Wait for MongoDB boot
 :: -------------------------------------------------
-timeout /t 3 /nobreak >nul
+timeout /t 5 /nobreak >nul
 
 :: -------------------------------------------------
 :: PM2
 :: -------------------------------------------------
-where pm2 >nul 2>&1
-
-if %errorLevel% neq 0 (
-    echo PM2 not found in PATH.
-    pause
-    exit /b
-)
-
-pm2 describe hms >nul 2>&1
+call pm2 describe hms >nul 2>&1
 
 if %errorLevel% equ 0 (
     echo Restarting HMS...
-    pm2 restart hms
+    call pm2 restart hms
 ) else (
     echo Starting HMS...
-    pm2 start server.js --name hms
+    call pm2 start server.js --name hms
 )
 
 :: -------------------------------------------------
@@ -81,10 +71,10 @@ if %errorLevel% equ 0 (
 :: -------------------------------------------------
 timeout /t 2 /nobreak >nul
 
-start "" chrome "http://192.168.0.10:8003/"
+start "" "http://192.168.0.10:8003/"
 
 echo =====================================
-echo All services started successfully.
+echo HMS Environment Started
 echo =====================================
 
 pause
